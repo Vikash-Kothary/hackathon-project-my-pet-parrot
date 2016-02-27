f30e162a7778b6d73e1cb26c10c434b8cdc73cfc
VikashKothary <kothary.vikash@gmail.com>
2016-02-27 23:07:08 +0000

    Added Parrot Sample code

--- app/src/main/java/com/robotpets/mypetparrot/MainActivity.java
+++ app/src/main/java/com/robotpets/mypetparrot/MainActivity.java
@@ -1,168 +1,257 @@
 package com.robotpets.mypetparrot;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
-public class MainActivity extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
+import com.parrot.arsdk.ardiscovery.ARDISCOVERY_PRODUCT_ENUM;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceService;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryService;
+import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiver;
+import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiverDelegate;
+import com.parrot.arsdk.arsal.ARSALPrint;
+import com.parrot.arsdk.arsal.ARSAL_PRINT_LEVEL_ENUM;
+import com.parrot.bebopdronepiloting.R;
+import com.robotpets.mypetparrot.PilotingActivity;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+import java.util.ArrayList;
+import java.util.List;
 
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
+public class MainActivity extends ActionBarActivity implements ARDiscoveryServicesDevicesListUpdatedReceiverDelegate
+{
+    private static String TAG = MainActivity.class.getSimpleName();
 
-    private View mContentView;
-    private View mControlsView;
-    private boolean mVisible;
+    static
+    {
+        try
+        {
+            System.loadLibrary("arsal");
+            System.loadLibrary("arsal_android");
+            System.loadLibrary("arnetworkal");
+            System.loadLibrary("arnetworkal_android");
+            System.loadLibrary("ardiscovery");
+            System.loadLibrary("ardiscovery_android");
+            System.loadLibrary("arcontroller");
+            System.loadLibrary("arcontroller_android");
+
+            ARSALPrint.setMinimumLogLevel(ARSAL_PRINT_LEVEL_ENUM.ARSAL_PRINT_INFO);
+        }
+        catch (Exception e)
+        {
+            Log.e(TAG, "Oops (LoadLibrary)", e);
+        }
+    }
+
+    private ListView listView ;
+    private List<ARDiscoveryDeviceService> deviceList;
+    private String[] deviceNameList;
+
+    private ARDiscoveryService ardiscoveryService;
+    private boolean ardiscoveryServiceBound = false;
+    private ServiceConnection ardiscoveryServiceConnection;
+    public IBinder discoveryServiceBinder;
+
+    private BroadcastReceiver ardiscoveryServicesDevicesListUpdatedReceiver;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        initBroadcastReceiver();
+        initServiceConnection();
+
+        listView = (ListView) findViewById(R.id.list);
+
+        deviceList = new ArrayList<ARDiscoveryDeviceService>();
+        deviceNameList = new String[]{};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, deviceNameList);
 
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+
             @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+                ARDiscoveryDeviceService service = deviceList.get(position);
+
+                Intent intent = new Intent(MainActivity.this, PilotingActivity.class);
+                intent.putExtra(PilotingActivity.EXTRA_DEVICE_SERVICE, service);
+
+
+                startActivity(intent);
+            }
+
+        });
+    }
+
+    private void initServices()
+    {
+        if (discoveryServiceBinder == null)
+        {
+            Intent i = new Intent(getApplicationContext(), ARDiscoveryService.class);
+            getApplicationContext().bindService(i, ardiscoveryServiceConnection, Context.BIND_AUTO_CREATE);
+        }
+        else
+        {
+            ardiscoveryService = ((ARDiscoveryService.LocalBinder) discoveryServiceBinder).getService();
+            ardiscoveryServiceBound = true;
+
+            ardiscoveryService.start();
+        }
+    }
+
+    private void closeServices()
+    {
+        Log.d(TAG, "closeServices ...");
+
+        if (ardiscoveryServiceBound)
+        {
+            new Thread(new Runnable() {
+                @Override
+                public void run()
+                {
+                    ardiscoveryService.stop();
+
+                    getApplicationContext().unbindService(ardiscoveryServiceConnection);
+                    ardiscoveryServiceBound = false;
+                    discoveryServiceBinder = null;
+                    ardiscoveryService = null;
+                }
+            }).start();
+        }
+    }
+
+    private void initBroadcastReceiver()
+    {
+        ardiscoveryServicesDevicesListUpdatedReceiver = new ARDiscoveryServicesDevicesListUpdatedReceiver(this);
+    }
+
+    private void initServiceConnection()
+    {
+        ardiscoveryServiceConnection = new ServiceConnection()
+        {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service)
+            {
+                discoveryServiceBinder = service;
+                ardiscoveryService = ((ARDiscoveryService.LocalBinder) service).getService();
+                ardiscoveryServiceBound = true;
+
+                ardiscoveryService.start();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name)
+            {
+                ardiscoveryService = null;
+                ardiscoveryServiceBound = false;
+            }
+        };
+    }
+
+    private void registerReceivers()
+    {
+        LocalBroadcastManager localBroadcastMgr = LocalBroadcastManager.getInstance(getApplicationContext());
+        localBroadcastMgr.registerReceiver(ardiscoveryServicesDevicesListUpdatedReceiver, new IntentFilter(ARDiscoveryService.kARDiscoveryServiceNotificationServicesDevicesListUpdated));
+
+    }
+
+    private void unregisterReceivers()
+    {
+        LocalBroadcastManager localBroadcastMgr = LocalBroadcastManager.getInstance(getApplicationContext());
+        localBroadcastMgr.unregisterReceiver(ardiscoveryServicesDevicesListUpdatedReceiver);
+    }
+
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        Log.d(TAG, "onResume ...");
+
+        onServicesDevicesListUpdated();
+
+        registerReceivers();
+
+        initServices();
+
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    public void onPause()
+    {
+        Log.d(TAG, "onPause ...");
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
+        unregisterReceivers();
+        closeServices();
+
+        super.onPause();
     }
 
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+    @Override
+    public void onServicesDevicesListUpdated()
+    {
+        Log.d(TAG, "onServicesDevicesListUpdated ...");
+
+        List<ARDiscoveryDeviceService> list;
+
+        if (ardiscoveryService != null)
+        {
+            list = ardiscoveryService.getDeviceServicesArray();
+
+            deviceList = new ArrayList<ARDiscoveryDeviceService> ();
+            List<String> deviceNames = new ArrayList<String>();
+
+            if(list != null)
+            {
+                for (ARDiscoveryDeviceService service : list)
+                {
+                    Log.e(TAG, "service :  "+ service + " name = " + service.getName());
+                    ARDISCOVERY_PRODUCT_ENUM product = ARDiscoveryService.getProductFromProductID(service.getProductID());
+                    Log.e(TAG, "product :  "+ product);
+                    // only display Rolling Spider drones
+                    if ((ARDISCOVERY_PRODUCT_ENUM.ARDISCOVERY_PRODUCT_MINIDRONE.equals(product)) ||
+                        (ARDISCOVERY_PRODUCT_ENUM.ARDISCOVERY_PRODUCT_MINIDRONE_EVO_BRICK.equals(product)) ||
+                        (ARDISCOVERY_PRODUCT_ENUM.ARDISCOVERY_PRODUCT_MINIDRONE_EVO_HYDROFOIL.equals(product)) ||
+                        (ARDISCOVERY_PRODUCT_ENUM.ARDISCOVERY_PRODUCT_MINIDRONE_EVO_LIGHT.equals(product)) )
+
+                    {
+                        deviceList.add(service);
+                        deviceNames.add(service.getName());
+                    }
+                }
             }
-            return false;
+
+            deviceNameList = deviceNames.toArray(new String[deviceNames.size()]);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, deviceNameList);
+
+            // Assign adapter to ListView
+            listView.setAdapter(adapter);
         }
-    };
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-
-    private final Handler mHideHandler = new Handler();
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 }
